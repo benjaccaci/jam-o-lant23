a3baec5fb25054c76ed2d33f589715feb2a5a6ec
LukTeg <tegge.l@northeastern.edu>
2023-10-23 19:54:18 -0400

    Implemented bouncy material

--- Assets/EnemyScript.cs
+++ Assets/EnemyScript.cs
@@ -7,11 +7,12 @@ public class EnemyScript : MonoBehaviour
     [SerializeField]
     private float moveSpeed;
     private Rigidbody2D rb;
-    public float angle = Mathf.PI / 3;
     private float previousXVel = 0;
     private float previousYVel = 0;
     public GameObject player;
     public GameObject flashlightLink;
+    //local value of rotation in radians
+    public float angle = 0;
 
     //distance between the player and enemy at which the enemy would charge directly at the player;
     [SerializeField]
@@ -35,60 +36,27 @@ public class EnemyScript : MonoBehaviour
         rb.AddForce(moveDirection, ForceMode2D.Impulse);
         previousXVel = rb.velocity.x;
         previousYVel = rb.velocity.y;
+        transform.rotation = Quaternion.Euler(0,0,angle*180/Mathf.PI);
     }
     void OnCollisionEnter2D(Collision2D coll)
     {
-        if (Vector3.Distance(player.transform.position, transform.position) < attackDistanceTrigger)
+        if (coll.gameObject == flashlightLink)
+        {
+            Debug.Log("Hit by Light");
+            angle += Mathf.PI;
+            rb.AddForce(new Vector2(rb.velocity.x * -2, rb.velocity.y * -2), ForceMode2D.Impulse);
+        }
+        else if(Vector3.Distance(player.transform.position, transform.position) < attackDistanceTrigger)
         {
             rb.AddForce(new Vector2(rb.velocity.x * -1, rb.velocity.y * -1), ForceMode2D.Impulse);
             angle = Mathf.Atan2((player.transform.position.y - transform.position.y), (player.transform.position.x - transform.position.x));
             Debug.Log("I'm chasing the player");
         }
-        else if (coll.gameObject == flashlightLink) 
-        {
-            Debug.Log("here");
-            angle += Mathf.PI;
-            rb.AddForce(new Vector2(rb.velocity.x * -10, rb.velocity.y * -10), ForceMode2D.Impulse);
-        }
         else
         {
             Debug.Log("I'm looking around.");
-            //Changed angle for enemyMovement
-            float deltaAngle = 0;
-            if (System.Math.Abs(rb.velocity.x) < 0.001)
-            {
-                if (previousXVel < 0)
-                {
-                    deltaAngle = Mathf.PI / -2;
-                }
-                else
-                {
-                    deltaAngle = Mathf.PI / 2;
-                }
-
-                if (rb.velocity.y < 0)
-                {
-                    deltaAngle *= -1;
-                }
-                rb.AddForce(new Vector2(0, rb.velocity.y * -1), ForceMode2D.Impulse);
-            }
-            else if (System.Math.Abs(rb.velocity.y) < 0.001)
-            {
-                if (previousYVel < 0)
-                {
-                    deltaAngle = Mathf.PI / -2;
-                }
-                else
-                {
-                    deltaAngle = Mathf.PI / 2;
-                }
-                if (rb.velocity.x > 0)
-                {
-                    deltaAngle *= -1;
-                }
-                rb.AddForce(new Vector2(rb.velocity.x * -1, 0), ForceMode2D.Impulse);
-            }
-            angle += deltaAngle;
+            rb.AddForce(new Vector2(rb.velocity.x / -2, rb.velocity.y / -2), ForceMode2D.Impulse);
+            angle = Mathf.Atan2(rb.velocity.y, rb.velocity.x);
         }
     }
 }
